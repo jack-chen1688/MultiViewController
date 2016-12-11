71425cea85d61327ae2c23ff5fd30c0b3c356e3b
Xuehua Chen <xuehuachen@Xuehuas-MacBook-Pro.local>
2016-12-10 20:54:49 -0800

    Added some project files

---
+++ MultiViewController/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MultiViewController
+//
+//  Created by Xuehua Chen on 12/10/16.
+//  Copyright © 2016 Xuehua Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("The first view is loaded!")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
